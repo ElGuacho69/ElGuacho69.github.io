90b0402feb4ecd3a44d14964f3162576437c8896
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-07-21 23:13:08 +0200

    Switched to absolute paths

--- statblocks/typescript/persistance/AnimalRepository.ts
+++ statblocks/typescript/persistance/AnimalRepository.ts
@@ -2,11 +2,11 @@ import { Animal } from '../logic/model/Animal';
 import { animalJsonToAnimal } from './AnimalMapper';
 
 export async function getAllAnimals(): Promise<Animal[]> {
-	const list: any = await fetchJson('../../animals/!list.json');
+	const list: any = await fetchJson('https://elguacho69.github.io/statblocks/animals/!list.json');
 	const jsonArray: any[] = [];
 	for(let i: number = 0; i < list.animals.length; i++) {
 		let animal = list.animals[i];
-		jsonArray.push(await fetchJson('../../animals/' + animal + '.json'))
+		jsonArray.push(await fetchJson('https://elguacho69.github.io/statblocks/animals/' + animal + '.json'))
 	}
 	const animalArray: Animal[] = jsonArray.map(animalJsonToAnimal);
 	return animalArray;
