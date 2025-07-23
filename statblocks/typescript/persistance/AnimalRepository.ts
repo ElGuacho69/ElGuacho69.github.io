f0de0d0d103f01be532758eff1f48481195de9cb
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-07-23 15:28:42 +0200

    Variablized repository url

--- statblocks/typescript/persistance/AnimalRepository.ts
+++ statblocks/typescript/persistance/AnimalRepository.ts
@@ -1,12 +1,14 @@
 import { Animal } from '../logic/model/Animal';
 import { animalJsonToAnimal } from './AnimalMapper';
 
+let repoURL: string = "https://elguacho69.github.io/statblocks";
+
 export async function getAllAnimals(): Promise<Animal[]> {
-	const list: any = await fetchJson('https://elguacho69.github.io/statblocks/animals/!list.json');
+	const list: any = await fetchJson(repoURL + '/animals/!list.json');
 	const jsonArray: any[] = [];
 	for(let i: number = 0; i < list.animals.length; i++) {
 		let animal = list.animals[i];
-		jsonArray.push(await fetchJson('https://elguacho69.github.io/statblocks/animals/' + animal + '.json'))
+		jsonArray.push(await fetchJson(repoURL + '/animals/' + animal + '.json'))
 	}
 	const animalArray: Animal[] = jsonArray.map(animalJsonToAnimal);
 	return animalArray;
